4437263e85d0354626888f2c89f8031793acb0d0
ASHWANI-KUMAR-JHA <ashwanijhaa77@gmail.com>
2026-01-20 17:50:54 +0530

    check if all good server is connected or not....

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Server is running on port 3000");
+const express=require('express');
+const dotenv=require('dotenv');
+
+const app = express();
+const port=process.env.PORT || 3000;
+
+
+app.get('/api/contacts',(req,res)=>{
+    res.send('Get all contacts');
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
